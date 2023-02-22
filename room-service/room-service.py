69e4ea778d69696ed1a0787398475de8bf500075
Luca Pasini <pasini.luca09@gmail.com>
2023-02-22 16:59:17 +0100

    Create first implementation of room-service

---
+++ room-service/room-service.py
@@ -0,0 +1,36 @@
+import json
+import paho.mqtt.client as paho
+from paho import mqtt
+from queue import Queue
+
+q = Queue()
+
+def on_subscribe(client, userdata, mid, granted_qos):
+    print("Subscribed: "+str(mid)+" "+str(granted_qos))
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+    decoded_message = msg.payload.decode("utf-8")
+    q.put(decoded_message)
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("occupance", qos=1)
+    
+def callback(client, userdata, message):
+    print("message received " ,str(message.payload.decode("utf-8")))
+
+client = paho.Client()
+client.on_connect = on_connect
+client.connect("broker.hivemq.com", 1883)
+client.on_subscribe = on_subscribe
+client.on_message = on_message
+
+client.loop_forever()
+    
+while True:
+    message = q.get()
+    if message is None:
+        print("No message")
+    else:
+        print(message)
