9ed081e2deb95b98781ed798827f2b3e632cdd27
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2023-02-23 21:20:16 +0100

    Create constants.h

---
+++ room-sensor-board/lib/help/constants.h
@@ -0,0 +1,13 @@
+#define sid ""
+#define pass ""
+
+/* wifi network info */
+
+const char* ssid = sid;
+const char* password = pass;
+
+/* MQTT server address */
+const char* mqtt_server = "broker.mqtt-dashboard.com";
+
+/* MQTT topic */
+const char* topic = "occupance";
