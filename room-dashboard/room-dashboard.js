6bdd88b3c45ab94b846262eaf83a2c06e311f7a7
Andrea Zavatta <zavattaandrea@gmail.com>
2023-02-23 02:14:10 +0100

    add google chart in history page
    Co-Authored-By: Marco Antolini <93138565+MarcoAntolini@users.noreply.github.com>

--- room-dashboard/room-dashboard.js
+++ room-dashboard/room-dashboard.js
@@ -1,9 +1,9 @@
 // TODO: controllare isNight dal json
 document.documentElement.setAttribute("data-theme", "dark");
-
 window.onload = function () {
+    google.charts.load('current', {'packages':['timeline']});
+    google.charts.setOnLoadCallback(drawChart);
     document.querySelector("body").style.transitionDuration = "1s";
-
     const slider = document.getElementById("slider");
     handleSlider(slider);
 
@@ -42,6 +42,22 @@ function handleSlider(slider) {
     });
 }
 
+
+function drawChart() {
+    var container = document.getElementById('timeline');
+    var chart = new google.visualization.Timeline(container);
+    var dataTable = new google.visualization.DataTable();
+
+    dataTable.addColumn({ type: 'string', id: 'President' });
+    dataTable.addColumn({ type: 'date', id: 'Start' });
+    dataTable.addColumn({ type: 'date', id: 'End' });
+    dataTable.addRows([
+      [ 'Washington', new Date(1789, 3, 30), new Date(1797, 2, 4) ],
+      [ 'Adams',      new Date(1797, 2, 4),  new Date(1801, 2, 4) ],
+      [ 'Jefferson',  new Date(1801, 2, 4),  new Date(1809, 2, 4) ]]);
+
+    chart.draw(dataTable);
+  }
 
 
 function slideRollerBlinds(value) {
