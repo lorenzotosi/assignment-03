6491897caf31e21da2a3a05db6ae6439d6f27901
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2023-02-24 15:46:22 +0100

    Update constants.h

--- room-sensor-board/lib/help/constants.h
+++ room-sensor-board/lib/help/constants.h
@@ -1,3 +1,7 @@
+#ifndef __CONSTANTS__
+#define __CONSTANTS__
+
+
 #include "pir.h"
 #include "led.h"
 #include "photoresistor.h"
@@ -8,8 +12,11 @@
 const char* ssid = "";
 const char* password = "";
 
+
 /* MQTT server address */
 const char* mqtt_server = "broker.mqtt-dashboard.com";
 
 /* MQTT topic */
 const char* topic = "occupance";
+
+#endif
