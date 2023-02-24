ee1cf2e3775e34f972e99ec95fef8606ac447ff9
Luca Pasini <pasini.luca09@gmail.com>
2023-02-24 13:08:30 +0100

    Add POST request

--- room-service/room-service.py
+++ room-service/room-service.py
@@ -1,18 +1,10 @@
 import json
 import paho.mqtt.client as paho
-from paho import mqtt
-from queue import Queue
-
-q = Queue()
+import requests as req
 
 def on_subscribe(client, userdata, mid, granted_qos):
     print("Subscribed: "+str(mid)+" "+str(granted_qos))
 
-def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
-    decoded_message = msg.payload.decode("utf-8")
-    msg = json.loads(decoded_message)
-    q.put(msg)
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -20,18 +12,21 @@ def on_connect(client, userdata, flags, rc):
     
 def callback(client, userdata, message):
     print("message received " ,str(message.payload.decode("utf-8")))
+    
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+    decoded_message = msg.payload.decode("utf-8")
+    msg = json.loads(decoded_message)
+    req.post(url, json=msg)
 
 client = paho.Client()
 client.on_connect = on_connect
 client.connect("broker.hivemq.com", 1883)
 client.on_subscribe = on_subscribe
 client.on_message = on_message
+url = "http://368d-176-247-145-118.eu.ngrok.io"
 
 client.loop_forever()
-    
-while True:
-    message = q.get()
-    if message is None:
-        print("No message")
-    else:
-        print(message)
+
+print("Starting loop")
+
