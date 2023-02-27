999267dc322e8a3b2fc008ffe9ba8aae48b7a71a
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2023-02-27 12:54:19 +0100

    reuse led class

---
+++ room-controller/lib/led/led.cpp
@@ -0,0 +1,28 @@
+#include "led.h"
+
+led::led(int pin) {
+    _pin = pin;
+    _state = false;
+}
+
+void led::initialize() {
+    pinMode(_pin, OUTPUT);
+}
+
+void led::on() {
+    digitalWrite(_pin, HIGH);
+    _state = true;
+}
+
+void led::off() {
+    digitalWrite(_pin, LOW);
+    _state = false;
+}
+
+void led::toggle() {
+    if (_state) {
+        off();
+    } else {
+        on();
+    }
+}
