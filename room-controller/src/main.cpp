2ccf3fa5c5863c08304ce9bccec0b8b1caf18b8c
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2023-03-02 19:28:15 +0100

    read from serial

--- room-controller/src/main.cpp
+++ room-controller/src/main.cpp
@@ -1,15 +1,22 @@
 #include "includes.h"
+#include "MsgService.h"
+
 
 Scheduler s;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  s.init(1000);
+  MsgService.init();
+  //s.init(1000);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  s.schedule();
+if (MsgService.isMsgAvailable()) {
+    Msg* msg = MsgService.receiveMsg();    
+    Serial.println(msg->getContent());
+    delete msg;
+  }
+  
 }
