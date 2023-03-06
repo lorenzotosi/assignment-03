a375fcfbb51c2bd58712fa22a99ad6a3a3629ec9
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2023-03-06 19:26:37 +0100

    testing

--- room-service/serial_test.py
+++ room-service/serial_test.py
@@ -2,17 +2,26 @@ import serial
 import time
 import json
 
-ser = serial.Serial("/dev/cu.usbmodem14201", 9600, timeout=0.1)
+ser = serial.Serial("/dev/cu.usbmodem14201", 9600, timeout=1)
 ser.close()
-ser.open()
+
 x = 1;
+y = 100;
 
 while True:
-    #ser.write(str(x).encode('utf-8'))
+    ser.open()
+    #ser.write(str(x).encode('ascii'))
     #ser.write(b'\n')
-    dictionary = {'d':x}
-    jsonString = json.dumps(dictionary, indent=4)
-    ser.write(jsonString.encode('utf-8'))
+    data = {}
+    data["operation"] = "sequenze"
+    data["luce"] = x
+    data["tapparelle"] = y
+    data=json.dumps(data)
+    print (data)
+    ser.write(data.encode('ascii'))
     ser.write(b'\n')
+    ser.flush()
+    ser.close()
     x = x + 1
+    y = y + 1
     time.sleep(1)
