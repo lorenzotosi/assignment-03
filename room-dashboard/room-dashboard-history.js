ddf6b652ce26f26409d33831601edb2458debdfd
MarcoAntolini <marco.antolini2001@gmail.com>
2023-03-14 00:04:02 +0100

    format code

--- room-dashboard/room-dashboard-history.js
+++ room-dashboard/room-dashboard-history.js
@@ -1,47 +1,53 @@
-const start = new Date().setHours(8, 0, 0);
-const end = new Date().setHours(19, 0, 0);
-let now = new Date();
-let isNight = start < now && now < end ? false : true;
-document.documentElement.setAttribute("data-theme", isNight ? "dark" : "light");
-let legendColor = isNight ? "white" : "black";
-let chartBackgroundColor = isNight ? "#686868" : "#c1e8ff";
+const start = new Date().setHours(8, 0, 0)
+const end = new Date().setHours(19, 0, 0)
+let now = new Date()
+let isNight = start < now && now < end ? false : true
+document.documentElement.setAttribute("data-theme", isNight ? "dark" : "light")
+let legendColor = isNight ? "white" : "black"
+let chartBackgroundColor = isNight ? "#686868" : "#c1e8ff"
 
 window.onload = function () {
-    google.charts.load("current", { "packages": ["timeline", "line", "corechart"] });
-    google.charts.setOnLoadCallback(drawChart);
-    document.querySelector("body").style.transitionDuration = "1s";
-    changeBackground(isNight);
-};
+    google.charts.load("current", {
+        packages: ["timeline", "line", "corechart"],
+    })
+    google.charts.setOnLoadCallback(drawChart)
+    document.querySelector("body").style.transitionDuration = "1s"
+    changeBackground(isNight)
+}
 
 window.onresize = function () {
-    google.charts.load("current", { "packages": ["timeline", "line", "corechart"] });
-    google.charts.setOnLoadCallback(drawChart);
-};
+    google.charts.load("current", {
+        packages: ["timeline", "line", "corechart"],
+    })
+    google.charts.setOnLoadCallback(drawChart)
+}
 
 function drawChart() {
     axios.get("logs.json").then((response) => {
-        let windowData = response.data["data"]["window-log"];
-        let lightsData = response.data["data"]["lights-log"];
-        drawWindowLog(windowData);
-        drawLightsLog(lightsData);
-        drawLightsUsage(lightsData);
+        let windowData = response.data["data"]["window-log"]
+        let lightsData = response.data["data"]["lights-log"]
+        drawWindowLog(windowData)
+        drawLightsLog(lightsData)
+        drawLightsUsage(lightsData)
         document.querySelectorAll("svg").forEach((element) => {
-            let divWidth = element.parentElement.parentElement.parentElement.parentElement.parentElement.offsetWidth * 0.002;
+            let divWidth =
+                element.parentElement.parentElement.parentElement.parentElement
+                    .parentElement.offsetWidth * 0.002
             if (divWidth < 0.96) {
-                element.setAttribute("style", `transform: scale(${divWidth})`);
+                element.setAttribute("style", `transform: scale(${divWidth})`)
             }
-        });
-    });
+        })
+    })
 }
 
 function drawWindowLog(data) {
-    const container = document.getElementById("window-log");
-    const chart = new google.visualization.LineChart(container);
-    const dataTable = new google.visualization.DataTable();
-    dataTable.addColumn({ type: "datetime", id: "Time" });
-    dataTable.addColumn({ type: "number", id: "Status" });
-    dataTable.addColumn({ type: "string", role: "tooltip", p: { html: true } });
-    data.forEach(element => {
+    const container = document.getElementById("window-log")
+    const chart = new google.visualization.LineChart(container)
+    const dataTable = new google.visualization.DataTable()
+    dataTable.addColumn({ type: "datetime", id: "Time" })
+    dataTable.addColumn({ type: "number", id: "Status" })
+    dataTable.addColumn({ type: "string", role: "tooltip", p: { html: true } })
+    data.forEach((element) => {
         const tooltip = (stage) => {
             let tooltipContent = `
                 <div>
@@ -49,29 +55,32 @@ function drawWindowLog(data) {
                         <p class="mb-0 text-dark"><b>Status:</b> ${element.status}%</p>
                     </div>
                     <div class="p-2" style="width: max-content">
-            `;
-            tooltipContent += stage == "start" ? `
+            `
+            tooltipContent +=
+                stage == "start"
+                    ? `
                         <p class="mb-0 text-dark"><b>Time:</b> ${element.start}</p>
-                        ` : `
+                        `
+                    : `
                         <p class="mb-0 text-dark"><b>Time:</b> ${element.end}</p>
-            `;
+            `
             tooltipContent += `
                     </div>
                 </div>
-            `;
-            return tooltipContent;
-        };
+            `
+            return tooltipContent
+        }
         dataTable.addRow([
             convertTime(element.start),
             element.status,
             tooltip("start"),
-        ]);
+        ])
         dataTable.addRow([
             convertTime(element.end),
             element.status,
             tooltip("end"),
-        ]);
-    });
+        ])
+    })
     const options = {
         colors: ["black"],
         curveType: "step",
@@ -80,11 +89,11 @@ function drawWindowLog(data) {
             height: "80%",
         },
         legend: {
-            position: "none"
+            position: "none",
         },
         backgroundColor: chartBackgroundColor,
         hAxis: {
-            format: 'HH:MM',
+            format: "HH:MM",
             textStyle: {
                 color: legendColor,
             },
@@ -99,43 +108,48 @@ function drawWindowLog(data) {
         tooltip: {
             isHtml: true,
         },
-    };
-    chart.draw(dataTable, options);
+    }
+    chart.draw(dataTable, options)
 }
 
 function drawLightsLog(data) {
-    const container = document.getElementById("lights-log");
-    const chart = new google.visualization.Timeline(container);
-    const dataTable = new google.visualization.DataTable();
-    dataTable.addColumn({ type: "string", id: "Status" });
-    dataTable.addColumn({ type: "string", id: "dummy bar label" });
-    dataTable.addColumn({ type: "string", role: "tooltip" });
-    dataTable.addColumn({ type: "datetime", id: "Start" });
-    dataTable.addColumn({ type: "datetime", id: "End" });
-    data.forEach(element => {
+    const container = document.getElementById("lights-log")
+    const chart = new google.visualization.Timeline(container)
+    const dataTable = new google.visualization.DataTable()
+    dataTable.addColumn({ type: "string", id: "Status" })
+    dataTable.addColumn({ type: "string", id: "dummy bar label" })
+    dataTable.addColumn({ type: "string", role: "tooltip" })
+    dataTable.addColumn({ type: "datetime", id: "Start" })
+    dataTable.addColumn({ type: "datetime", id: "End" })
+    data.forEach((element) => {
         const tooltip = `
             <div>
                 <div class="border-bottom p-2">
-                    <p class="mb-0 text-dark"><b>Status:</b> ${element.status}</p>
+                    <p class="mb-0 text-dark"><b>Status:</b> ${
+                        element.status
+                    }</p>
                 </div>
                 <div class="p-2" style="width: max-content">
-                    <p class="mb-0 text-dark"><b>Duration:</b> ${getTimeDifference(convertTime(element.end), convertTime(element.start))}</p>
+                    <p class="mb-0 text-dark"><b>Duration:</b> ${getTimeDifference(
+                        convertTime(element.end),
+                        convertTime(element.start)
+                    )}</p>
                 </div>
             </div>
-        `;
+        `
         dataTable.addRow([
             element.status,
-            '',
+            "",
             tooltip,
             convertTime(element.start),
             convertTime(element.end),
-        ]);
-    });
+        ])
+    })
     const options = {
         backgroundColor: chartBackgroundColor,
         colors: ["green", "red"],
         hAxis: {
-            format: 'HH:MM',
+            format: "HH:MM",
             gridlines: {
                 color: "transparent",
             },
@@ -150,24 +164,38 @@ function drawLightsLog(data) {
                 fontSize: 14,
             },
         },
-    };
-    chart.draw(dataTable, options);
+    }
+    chart.draw(dataTable, options)
 }
 
 function drawLightsUsage(data) {
-    const container = document.getElementById("lights-usage");
-    const chart = new google.visualization.PieChart(container);
-    let sumOn = "00:00:00";
-    let sumOff = "00:00:00";
-    data.forEach(element => {
-        if (element.status == "On") sumOn = sumTimes(sumOn, getTimeDifference(convertTime(element.end), convertTime(element.start)));
-        else sumOff = sumTimes(sumOff, getTimeDifference(convertTime(element.end), convertTime(element.start)));
-    });
+    const container = document.getElementById("lights-usage")
+    const chart = new google.visualization.PieChart(container)
+    let sumOn = "00:00:00"
+    let sumOff = "00:00:00"
+    data.forEach((element) => {
+        if (element.status == "On")
+            sumOn = sumTimes(
+                sumOn,
+                getTimeDifference(
+                    convertTime(element.end),
+                    convertTime(element.start)
+                )
+            )
+        else
+            sumOff = sumTimes(
+                sumOff,
+                getTimeDifference(
+                    convertTime(element.end),
+                    convertTime(element.start)
+                )
+            )
+    })
     const dataTable = google.visualization.arrayToDataTable([
         ["Status", "Hours"],
         ["On: " + sumOn, stringToSeconds(sumOn)],
-        ["Off: " + sumOff, stringToSeconds(sumOff)]
-    ]);
+        ["Off: " + sumOff, stringToSeconds(sumOff)],
+    ])
     const options = {
         backgroundColor: "transparent",
         colors: ["green", "red"],
@@ -191,57 +219,65 @@ function drawLightsUsage(data) {
         },
         tooltip: {
             trigger: "none",
-        }
-    };
-    chart.draw(dataTable, options);
+        },
+    }
+    chart.draw(dataTable, options)
 }
 
 function convertTime(timeString) {
-    const [hours, minutes, seconds] = timeString.split(":").map(component => parseInt(component));
-    const dateTime = new Date();
-    dateTime.setHours(hours, minutes, seconds);
-    return dateTime;
+    const [hours, minutes, seconds] = timeString
+        .split(":")
+        .map((component) => parseInt(component))
+    const dateTime = new Date()
+    dateTime.setHours(hours, minutes, seconds)
+    return dateTime
 }
 
 function stringToSeconds(timeString) {
-    const [hours, minutes, seconds] = timeString.split(":").map(component => parseInt(component));
-    return hours * 3600 + minutes * 60 + seconds;
+    const [hours, minutes, seconds] = timeString
+        .split(":")
+        .map((component) => parseInt(component))
+    return hours * 3600 + minutes * 60 + seconds
 }
 
 function sumTimes(firstTime, secondTime) {
-    const endSeconds = stringToSeconds(firstTime);
-    const startSeconds = stringToSeconds(secondTime);
-    const totalSeconds = endSeconds + startSeconds;
-    const hours = Math.floor(totalSeconds / 3600);
-    const minutes = Math.floor((totalSeconds % 3600) / 60);
-    const seconds = totalSeconds % 60;
-    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    const endSeconds = stringToSeconds(firstTime)
+    const startSeconds = stringToSeconds(secondTime)
+    const totalSeconds = endSeconds + startSeconds
+    const hours = Math.floor(totalSeconds / 3600)
+    const minutes = Math.floor((totalSeconds % 3600) / 60)
+    const seconds = totalSeconds % 60
+    return `${hours.toString().padStart(2, "0")}:${minutes
+        .toString()
+        .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`
 }
 
 function getTimeDifference(endDate, startDate) {
-    const millisDifference = Math.abs(endDate - startDate);
-    const secondsDifference = Math.round(millisDifference / 1000);
-    const hours = Math.floor(secondsDifference / 3660);
-    const minutes = Math.floor((secondsDifference % 3600) / 60);
-    const seconds = secondsDifference % 60;
-    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    const millisDifference = Math.abs(endDate - startDate)
+    const secondsDifference = Math.round(millisDifference / 1000)
+    const hours = Math.floor(secondsDifference / 3660)
+    const minutes = Math.floor((secondsDifference % 3600) / 60)
+    const seconds = secondsDifference % 60
+    return `${hours.toString().padStart(2, "0")}:${minutes
+        .toString()
+        .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`
 }
 
 function changeBackground(isNight) {
-    const nav = document.querySelector("nav");
+    const nav = document.querySelector("nav")
     if (isNight) {
-        document.documentElement.setAttribute("data-theme", "dark");
-        nav.classList.remove("bg-light");
-        nav.classList.add("navbar-dark");
-        nav.classList.add("bg-dark");
-        legendColor = "white";
-        chartBackgroundColor = "#686868";
+        document.documentElement.setAttribute("data-theme", "dark")
+        nav.classList.remove("bg-light")
+        nav.classList.add("navbar-dark")
+        nav.classList.add("bg-dark")
+        legendColor = "white"
+        chartBackgroundColor = "#686868"
     } else {
-        document.documentElement.setAttribute("data-theme", "light");
-        nav.classList.remove("bg-dark");
-        nav.classList.remove("navbar-dark");
-        nav.classList.add("bg-light");
-        legendColor = "black";
-        chartBackgroundColor = "#c1e8ff";
+        document.documentElement.setAttribute("data-theme", "light")
+        nav.classList.remove("bg-dark")
+        nav.classList.remove("navbar-dark")
+        nav.classList.add("bg-light")
+        legendColor = "black"
+        chartBackgroundColor = "#c1e8ff"
     }
-};
+}
--- room-dashboard/room-dashboard-window.js
+++ room-dashboard/room-dashboard-window.js
@@ -1,117 +1,147 @@
-const start = new Date().setHours(8, 0, 0);
-const end = new Date().setHours(19, 0, 0);
-let now = new Date();
-let isNight = start < now && now < end ? false : true;
-document.documentElement.setAttribute("data-theme", isNight ? "dark" : "light");
+const start = new Date().setHours(8, 0, 0)
+const end = new Date().setHours(19, 0, 0)
+let now = new Date()
+let isNight = start < now && now < end ? false : true
+document.documentElement.setAttribute("data-theme", isNight ? "dark" : "light")
 
 window.onload = function () {
-    document.querySelector("body").style.transitionDuration = "1s";
-    updateClock();
-    const slider = document.getElementById("slider");
-    const lightSwitch = document.querySelector(".light");
+    document.querySelector("body").style.transitionDuration = "1s"
+    updateClock()
+    const slider = document.getElementById("slider")
+    const lightSwitch = document.querySelector(".light")
     setupSystem(slider).then((response) => {
-        let isLightsOn = response;
+        let isLightsOn = response
         lightSwitch.addEventListener("click", function () {
-            isLightsOn = !isLightsOn;
-            checkLights(isLightsOn);
-        });
-    });
-    handleSlider(slider);
-    changeBackground(isNight);
-};
+            isLightsOn = !isLightsOn
+            checkLights(isLightsOn)
+        })
+    })
+    handleSlider(slider)
+    changeBackground(isNight)
+}
 
 function setupSystem(slider) {
     return axios.get("logs.json").then((response) => {
-        let windowData = response.data["data"]["window-log"];
-        let lightsData = response.data["data"]["lights-log"];
-        slider.defaultValue = windowData[windowData.length - 1].status;
-        slideRollerBlinds(slider.defaultValue);
-        return lightsData[lightsData.length - 1].status == "On" ? true : false;
-    });
+        let windowData = response.data["data"]["window-log"]
+        let lightsData = response.data["data"]["lights-log"]
+        slider.defaultValue = windowData[windowData.length - 1].status
+        slideRollerBlinds(slider.defaultValue)
+        return lightsData[lightsData.length - 1].status == "On" ? true : false
+    })
 }
 
 function handleSlider(slider) {
     slider.addEventListener("mousedown", function () {
-        document.getElementById("close").style.transition = "none";
+        document.getElementById("close").style.transition = "none"
         slider.addEventListener("mousemove", function () {
-            slideRollerBlinds(this.value);
-        });
-    });
+            slideRollerBlinds(this.value)
+        })
+    })
     slider.addEventListener("mouseup", function () {
-        document.getElementById("close").style.transition = "top 0.5s";
+        document.getElementById("close").style.transition = "top 0.5s"
         slider.removeEventListener("mousemove", function () {
-            slideRollerBlinds(this.value);
-        });
-        updateWindow(this.value);
-    });
+            slideRollerBlinds(this.value)
+        })
+        updateWindow(this.value)
+    })
     slider.addEventListener("click", function () {
-        slideRollerBlinds(this.value);
-    });
+        slideRollerBlinds(this.value)
+    })
 }
 
 function slideRollerBlinds(value) {
-    document.getElementById("close").style.top = -value * 0.85 + "%";
+    document.getElementById("close").style.top = -value * 0.85 + "%"
     if (value == 0) {
-        document.getElementById("window-status").innerHTML = "closed";
+        document.getElementById("window-status").innerHTML = "closed"
     } else if (value == 100) {
-        document.getElementById("window-status").innerHTML = "completely open";
+        document.getElementById("window-status").innerHTML = "completely open"
     } else {
-        document.getElementById("window-status").innerHTML = "open at " + value + "%";
+        document.getElementById("window-status").innerHTML =
+            "open at " + value + "%"
     }
 }
 
 function updateWindow(value) {
     const request = {
-        "content": {
-            "status": parseInt(value),
-            "start": new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' }),
-            "end": new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' }),
+        content: {
+            status: parseInt(value),
+            start: new Date().toLocaleTimeString([], {
+                hour: "2-digit",
+                minute: "2-digit",
+                second: "2-digit",
+            }),
+            end: new Date().toLocaleTimeString([], {
+                hour: "2-digit",
+                minute: "2-digit",
+                second: "2-digit",
+            }),
         },
-        "type": "window",
-    };
-    axios.post("room-dashboard-history.php", request).then();
+        type: "window",
+    }
+    axios.post("room-dashboard-history.php", request).then()
 }
 
 function checkLights(isLightsOn) {
-    document.getElementById("lights-status").innerHTML = isLightsOn ? "on" : "off";
-    document.getElementById("lights-status").style.color = isLightsOn ? "green" : "red";
-    document.getElementById(isLightsOn ? "light-on" : "light-off").style.opacity = "1";
-    document.getElementById(isLightsOn ? "light-off" : "light-on").style.opacity = "0";
+    document.getElementById("lights-status").innerHTML = isLightsOn
+        ? "on"
+        : "off"
+    document.getElementById("lights-status").style.color = isLightsOn
+        ? "green"
+        : "red"
+    document.getElementById(
+        isLightsOn ? "light-on" : "light-off"
+    ).style.opacity = "1"
+    document.getElementById(
+        isLightsOn ? "light-off" : "light-on"
+    ).style.opacity = "0"
     const request = {
-        "content": {
-            "status": isLightsOn ? "On" : "Off",
-            "start": new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' }),
-            "end": new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' }),
+        content: {
+            status: isLightsOn ? "On" : "Off",
+            start: new Date().toLocaleTimeString([], {
+                hour: "2-digit",
+                minute: "2-digit",
+                second: "2-digit",
+            }),
+            end: new Date().toLocaleTimeString([], {
+                hour: "2-digit",
+                minute: "2-digit",
+                second: "2-digit",
+            }),
         },
-        "type": "lights",
-    };
-    axios.post("room-dashboard-history.php", request).then();
+        type: "lights",
+    }
+    axios.post("room-dashboard-history.php", request).then()
 }
 
 function changeBackground(isNight) {
-    const night = document.getElementById("night");
-    const day = document.getElementById("day");
-    const nav = document.querySelector("nav");
+    const night = document.getElementById("night")
+    const day = document.getElementById("day")
+    const nav = document.querySelector("nav")
     if (isNight) {
-        document.documentElement.setAttribute("data-theme", "dark");
-        day.style.opacity = "0";
-        night.style.opacity = "1";
-        nav.classList.remove("bg-light");
-        nav.classList.add("navbar-dark");
-        nav.classList.add("bg-dark");
+        document.documentElement.setAttribute("data-theme", "dark")
+        day.style.opacity = "0"
+        night.style.opacity = "1"
+        nav.classList.remove("bg-light")
+        nav.classList.add("navbar-dark")
+        nav.classList.add("bg-dark")
     } else {
-        document.documentElement.setAttribute("data-theme", "light");
-        night.style.opacity = "0";
-        day.style.opacity = "1";
-        nav.classList.remove("bg-dark");
-        nav.classList.remove("navbar-dark");
-        nav.classList.add("bg-light");
+        document.documentElement.setAttribute("data-theme", "light")
+        night.style.opacity = "0"
+        day.style.opacity = "1"
+        nav.classList.remove("bg-dark")
+        nav.classList.remove("navbar-dark")
+        nav.classList.add("bg-light")
     }
 }
 
 function updateClock() {
-    const clock = document.getElementById("clock");
-    let currentTime = new Date().toLocaleTimeString("en-US", { hour12: false, hour: "2-digit", minute: "2-digit", second: "2-digit" });
-    clock.innerHTML = currentTime;
+    const clock = document.getElementById("clock")
+    let currentTime = new Date().toLocaleTimeString("en-US", {
+        hour12: false,
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+    })
+    clock.innerHTML = currentTime
 }
-setInterval(updateClock, 1000);
+setInterval(updateClock, 1000)
