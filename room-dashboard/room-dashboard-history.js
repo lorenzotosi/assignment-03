c16c2596a8a91c5e0f6a664c4ce7cfcaf408e117
MarcoAntolini <marco.antolini2001@gmail.com>
2023-03-14 01:18:16 +0100

    fix logs

--- room-dashboard/room-dashboard-history.js
+++ room-dashboard/room-dashboard-history.js
@@ -4,13 +4,14 @@ let now = new Date()
 let isNight = start < now && now < end ? false : true
 document.documentElement.setAttribute("data-theme", isNight ? "dark" : "light")
 let legendColor = isNight ? "white" : "black"
-let chartBackgroundColor = isNight ? "#686868" : "#c1e8ff"
 
 window.onload = function () {
     google.charts.load("current", {
         packages: ["timeline", "line", "corechart"],
     })
-    google.charts.setOnLoadCallback(drawChart)
+    google.charts.setOnLoadCallback(function () {
+        drawChart(false)
+    })
     document.querySelector("body").style.transitionDuration = "1s"
     changeBackground(isNight)
 }
@@ -19,16 +20,20 @@ window.onresize = function () {
     google.charts.load("current", {
         packages: ["timeline", "line", "corechart"],
     })
-    google.charts.setOnLoadCallback(drawChart)
+    google.charts.setOnLoadCallback(function () {
+        drawChart(true)
+    })
 }
 
-function drawChart() {
+function drawChart(isReload) {
     axios.get("logs.json").then((response) => {
         let windowData = response.data["data"]["window-log"]
         let lightsData = response.data["data"]["lights-log"]
-        drawWindowLog(windowData)
-        drawLightsLog(lightsData)
         drawLightsUsage(lightsData)
+        if (!isReload) {
+            drawWindowLog(windowData)
+            drawLightsLog(lightsData)
+        }
         document.querySelectorAll("svg").forEach((element) => {
             let divWidth =
                 element.parentElement.parentElement.parentElement.parentElement
@@ -42,136 +47,36 @@ function drawChart() {
 
 function drawWindowLog(data) {
     const container = document.getElementById("window-log")
-    const chart = new google.visualization.LineChart(container)
-    const dataTable = new google.visualization.DataTable()
-    dataTable.addColumn({ type: "datetime", id: "Time" })
-    dataTable.addColumn({ type: "number", id: "Status" })
-    dataTable.addColumn({ type: "string", role: "tooltip", p: { html: true } })
     data.forEach((element) => {
-        const tooltip = (stage) => {
-            let tooltipContent = `
-                <div>
-                    <div class="border-bottom p-2">
-                        <p class="mb-0 text-dark"><b>Status:</b> ${element.status}%</p>
-                    </div>
-                    <div class="p-2" style="width: max-content">
-            `
-            tooltipContent +=
-                stage == "start"
-                    ? `
-                        <p class="mb-0 text-dark"><b>Time:</b> ${element.start}</p>
-                        `
-                    : `
-                        <p class="mb-0 text-dark"><b>Time:</b> ${element.end}</p>
-            `
-            tooltipContent += `
-                    </div>
-                </div>
-            `
-            return tooltipContent
-        }
-        dataTable.addRow([
-            convertTime(element.start),
-            element.status,
-            tooltip("start"),
-        ])
-        dataTable.addRow([
-            convertTime(element.end),
-            element.status,
-            tooltip("end"),
-        ])
+        let div = document.createElement("div")
+        div.setAttribute("class", "d-flex justify-content-between")
+        let time = document.createElement("p")
+        time.setAttribute("class", "mb-0")
+        time.innerHTML = element.start
+        let status = document.createElement("p")
+        status.setAttribute("class", "mb-0")
+        status.innerHTML = element.status + "%"
+        div.appendChild(time)
+        div.appendChild(status)
+        container.appendChild(div)
     })
-    const options = {
-        colors: ["black"],
-        curveType: "step",
-        chartArea: {
-            width: "85%",
-            height: "80%",
-        },
-        legend: {
-            position: "none",
-        },
-        backgroundColor: chartBackgroundColor,
-        hAxis: {
-            format: "HH:MM",
-            textStyle: {
-                color: legendColor,
-            },
-            viewWindow: {
-                min: new Date().setHours(0, 0),
-                max: new Date().setHours(23, 59),
-            },
-        },
-        vAxis: {
-            textStyle: {
-                color: legendColor,
-            },
-            ticks: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
-        },
-        lineWidth: 3,
-        tooltip: {
-            isHtml: true,
-        },
-    }
-    chart.draw(dataTable, options)
 }
 
 function drawLightsLog(data) {
     const container = document.getElementById("lights-log")
-    const chart = new google.visualization.Timeline(container)
-    const dataTable = new google.visualization.DataTable()
-    dataTable.addColumn({ type: "string", id: "Status" })
-    dataTable.addColumn({ type: "string", id: "dummy bar label" })
-    dataTable.addColumn({ type: "string", role: "tooltip" })
-    dataTable.addColumn({ type: "datetime", id: "Start" })
-    dataTable.addColumn({ type: "datetime", id: "End" })
     data.forEach((element) => {
-        const tooltip = `
-            <div>
-                <div class="border-bottom p-2">
-                    <p class="mb-0 text-dark"><b>Status:</b> ${
-                        element.status
-                    }</p>
-                </div>
-                <div class="p-2" style="width: max-content">
-                    <p class="mb-0 text-dark"><b>Duration:</b> ${getTimeDifference(
-                        convertTime(element.end),
-                        convertTime(element.start)
-                    )}</p>
-                </div>
-            </div>
-        `
-        dataTable.addRow([
-            element.status,
-            "",
-            tooltip,
-            convertTime(element.start),
-            convertTime(element.end),
-        ])
+        let div = document.createElement("div")
+        div.setAttribute("class", "d-flex justify-content-between")
+        let time = document.createElement("p")
+        time.setAttribute("class", "mb-0")
+        time.innerHTML = element.start
+        let status = document.createElement("p")
+        status.setAttribute("class", "mb-0")
+        status.innerHTML = element.status
+        div.appendChild(time)
+        div.appendChild(status)
+        container.appendChild(div)
     })
-    const options = {
-        backgroundColor: chartBackgroundColor,
-        colors: ["green", "red"],
-        hAxis: {
-            format: "HH:MM",
-            gridlines: {
-                color: "transparent",
-            },
-            minValue: new Date().setHours(0, 0, 0),
-            maxValue: new Date().setHours(23, 59, 59),
-        },
-        timeline: {
-            rowLabelStyle: {
-                color: "black",
-                fontSize: 14,
-            },
-            barLabelStyle: {
-                color: legendColor,
-                fontSize: 14,
-            },
-        },
-    }
-    chart.draw(dataTable, options)
 }
 
 function drawLightsUsage(data) {
@@ -277,13 +182,11 @@ function changeBackground(isNight) {
         nav.classList.add("navbar-dark")
         nav.classList.add("bg-dark")
         legendColor = "white"
-        chartBackgroundColor = "#686868"
     } else {
         document.documentElement.setAttribute("data-theme", "light")
         nav.classList.remove("bg-dark")
         nav.classList.remove("navbar-dark")
         nav.classList.add("bg-light")
         legendColor = "black"
-        chartBackgroundColor = "#c1e8ff"
     }
 }
