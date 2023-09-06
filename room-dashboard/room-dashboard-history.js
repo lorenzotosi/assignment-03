263573954a30be16f761916dae89395629c21fa6
MarcoAntolini <marco.antolini2001@gmail.com>
2023-09-06 23:44:11 +0200

    fix some styling

--- room-dashboard/room-dashboard-history.js
+++ room-dashboard/room-dashboard-history.js
@@ -1,196 +1,178 @@
 const start = new Date().setHours(8, 0, 0)
 const end = new Date().setHours(19, 0, 0)
 let now = new Date()
-let isNight = start < now && now < end ? false : true
+let isNight = !(start < now && now < end)
 document.documentElement.setAttribute("data-theme", isNight ? "dark" : "light")
 let legendColor = isNight ? "white" : "black"
 
 window.onload = function () {
-    google.charts.load("current", {
-        packages: ["timeline", "line", "corechart"],
-    })
-    google.charts.setOnLoadCallback(function () {
-        drawChart(false)
-    })
-    document.querySelector("body").style.transitionDuration = "1s"
-    changeBackground(isNight)
-    setTimeout(function () {
-        document.querySelector(".preload").style.opacity = "0"
-        document.querySelector(".preload").style.zIndex = "-1"
-    }, 500)
+	google.charts.load("current", {
+		packages: ["timeline", "line", "corechart"]
+	})
+	google.charts.setOnLoadCallback(function () {
+		drawChart(false)
+	})
+	document.querySelector("body").style.transitionDuration = "1s"
+	changeBackground(isNight)
+	setTimeout(function () {
+		document.querySelector(".preload").style.opacity = "0"
+		document.querySelector(".preload").style.zIndex = "-1"
+	}, 500)
 }
 
 window.onresize = function () {
-    google.charts.load("current", {
-        packages: ["timeline", "line", "corechart"],
-    })
-    google.charts.setOnLoadCallback(function () {
-        drawChart(true)
-    })
+	google.charts.load("current", {
+		packages: ["timeline", "line", "corechart"]
+	})
+	google.charts.setOnLoadCallback(function () {
+		drawChart(true)
+	})
 }
 
 function drawChart(isReload) {
-    axios.get("logs.json").then((response) => {
-        let windowData = response.data["data"]["window-log"]
-        let lightsData = response.data["data"]["lights-log"]
-        drawLightsUsage(lightsData)
-        if (!isReload) {
-            drawWindowLog(windowData)
-            drawLightsLog(lightsData)
-        }
-        document.querySelectorAll("svg").forEach((element) => {
-            let divWidth =
-                element.parentElement.parentElement.parentElement.parentElement
-                    .parentElement.offsetWidth * 0.002
-            if (divWidth < 0.96) {
-                element.setAttribute("style", `transform: scale(${divWidth})`)
-            }
-        })
-    })
+	axios.get("logs.json").then(response => {
+		let windowData = response.data["data"]["window-log"]
+		let lightsData = response.data["data"]["lights-log"]
+		drawLightsUsage(lightsData)
+		if (!isReload) {
+			drawWindowLog(windowData)
+			drawLightsLog(lightsData)
+		}
+		document.querySelectorAll("svg").forEach(element => {
+			let divWidth =
+				element.parentElement.parentElement.parentElement.parentElement.parentElement.offsetWidth * 0.002
+			if (divWidth < 0.96) {
+				element.setAttribute("style", `transform: scale(${divWidth})`)
+			}
+		})
+	})
 }
 
 function drawWindowLog(data) {
-    const container = document.getElementById("window-log")
-    data.forEach((element) => {
-        let div = document.createElement("div")
-        div.setAttribute("class", "d-flex justify-content-between")
-        let time = document.createElement("p")
-        time.setAttribute("class", "mb-0")
-        time.innerHTML = element.start
-        let status = document.createElement("p")
-        status.setAttribute("class", "mb-0")
-        status.innerHTML = element.status + "%"
-        div.appendChild(time)
-        div.appendChild(status)
-        container.appendChild(div)
-    })
+	const container = document.getElementById("window-log")
+	data.forEach(element => {
+		let div = document.createElement("div")
+		div.setAttribute("class", "d-flex justify-content-between gap-2")
+		let time = document.createElement("p")
+		time.setAttribute("class", "mb-0")
+		time.innerHTML = element.start
+		let status = document.createElement("p")
+		status.setAttribute("class", "mb-0")
+		status.innerHTML = element.status + "%"
+		div.appendChild(time)
+		div.appendChild(status)
+		container.appendChild(div)
+	})
 }
 
 function drawLightsLog(data) {
-    const container = document.getElementById("lights-log")
-    data.forEach((element) => {
-        let div = document.createElement("div")
-        div.setAttribute("class", "d-flex justify-content-between")
-        let time = document.createElement("p")
-        time.setAttribute("class", "mb-0")
-        time.innerHTML = element.start
-        let status = document.createElement("p")
-        status.setAttribute("class", "mb-0")
-        status.innerHTML = element.status
-        div.appendChild(time)
-        div.appendChild(status)
-        container.appendChild(div)
-    })
+	const container = document.getElementById("lights-log")
+	data.forEach(element => {
+		let div = document.createElement("div")
+		div.setAttribute("class", "d-flex justify-content-between gap-2")
+		let time = document.createElement("p")
+		time.setAttribute("class", "mb-0")
+		time.innerHTML = element.start
+		let status = document.createElement("p")
+		status.setAttribute("class", "mb-0")
+		status.innerHTML = element.status
+		div.appendChild(time)
+		div.appendChild(status)
+		container.appendChild(div)
+	})
 }
 
 function drawLightsUsage(data) {
-    const container = document.getElementById("lights-usage")
-    const chart = new google.visualization.PieChart(container)
-    let sumOn = "00:00:00"
-    let sumOff = "00:00:00"
-    data.forEach((element) => {
-        if (element.status == "On")
-            sumOn = sumTimes(
-                sumOn,
-                getTimeDifference(
-                    convertTime(element.end),
-                    convertTime(element.start)
-                )
-            )
-        else
-            sumOff = sumTimes(
-                sumOff,
-                getTimeDifference(
-                    convertTime(element.end),
-                    convertTime(element.start)
-                )
-            )
-    })
-    const dataTable = google.visualization.arrayToDataTable([
-        ["Status", "Hours"],
-        ["On: " + sumOn, stringToSeconds(sumOn)],
-        ["Off: " + sumOff, stringToSeconds(sumOff)],
-    ])
-    const options = {
-        backgroundColor: "transparent",
-        colors: ["green", "red"],
-        is3D: true,
-        legend: {
-            position: "right",
-            textStyle: {
-                color: legendColor,
-                fontSize: 14,
-            },
-        },
-        chartArea: {
-            left: 0,
-            top: 0,
-            width: "100%",
-            height: "100%",
-        },
-        pieSliceText: "percentage",
-        pieSliceTextStyle: {
-            fontSize: 14,
-        },
-        tooltip: {
-            trigger: "none",
-        },
-    }
-    chart.draw(dataTable, options)
+	const container = document.getElementById("lights-usage")
+	const chart = new google.visualization.PieChart(container)
+	let sumOn = "00:00:00"
+	let sumOff = "00:00:00"
+	data.forEach(element => {
+		if (element.status == "On")
+			sumOn = sumTimes(sumOn, getTimeDifference(convertTime(element.end), convertTime(element.start)))
+		else sumOff = sumTimes(sumOff, getTimeDifference(convertTime(element.end), convertTime(element.start)))
+	})
+	const dataTable = google.visualization.arrayToDataTable([
+		["Status", "Hours"],
+		["On: " + sumOn, stringToSeconds(sumOn)],
+		["Off: " + sumOff, stringToSeconds(sumOff)]
+	])
+	const options = {
+		backgroundColor: "transparent",
+		colors: ["green", "red"],
+		is3D: true,
+		legend: {
+			position: "right",
+			textStyle: {
+				color: legendColor,
+				fontSize: 14
+			}
+		},
+		chartArea: {
+			left: 0,
+			top: 0,
+			width: "100%",
+			height: "100%"
+		},
+		pieSliceText: "percentage",
+		pieSliceTextStyle: {
+			fontSize: 14
+		},
+		tooltip: {
+			trigger: "none"
+		}
+	}
+	chart.draw(dataTable, options)
 }
 
 function convertTime(timeString) {
-    const [hours, minutes, seconds] = timeString
-        .split(":")
-        .map((component) => parseInt(component))
-    const dateTime = new Date()
-    dateTime.setHours(hours, minutes, seconds)
-    return dateTime
+	const [hours, minutes, seconds] = timeString.split(":").map(component => parseInt(component))
+	const dateTime = new Date()
+	dateTime.setHours(hours, minutes, seconds)
+	return dateTime
 }
 
 function stringToSeconds(timeString) {
-    const [hours, minutes, seconds] = timeString
-        .split(":")
-        .map((component) => parseInt(component))
-    return hours * 3600 + minutes * 60 + seconds
+	const [hours, minutes, seconds] = timeString.split(":").map(component => parseInt(component))
+	return hours * 3600 + minutes * 60 + seconds
 }
 
 function sumTimes(firstTime, secondTime) {
-    const endSeconds = stringToSeconds(firstTime)
-    const startSeconds = stringToSeconds(secondTime)
-    const totalSeconds = endSeconds + startSeconds
-    const hours = Math.floor(totalSeconds / 3600)
-    const minutes = Math.floor((totalSeconds % 3600) / 60)
-    const seconds = totalSeconds % 60
-    return `${hours.toString().padStart(2, "0")}:${minutes
-        .toString()
-        .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`
+	const endSeconds = stringToSeconds(firstTime)
+	const startSeconds = stringToSeconds(secondTime)
+	const totalSeconds = endSeconds + startSeconds
+	const hours = Math.floor(totalSeconds / 3600)
+	const minutes = Math.floor((totalSeconds % 3600) / 60)
+	const seconds = totalSeconds % 60
+	return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds
+		.toString()
+		.padStart(2, "0")}`
 }
 
 function getTimeDifference(endDate, startDate) {
-    const millisDifference = Math.abs(endDate - startDate)
-    const secondsDifference = Math.round(millisDifference / 1000)
-    const hours = Math.floor(secondsDifference / 3660)
-    const minutes = Math.floor((secondsDifference % 3600) / 60)
-    const seconds = secondsDifference % 60
-    return `${hours.toString().padStart(2, "0")}:${minutes
-        .toString()
-        .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`
+	const millisDifference = Math.abs(endDate - startDate)
+	const secondsDifference = Math.round(millisDifference / 1000)
+	const hours = Math.floor(secondsDifference / 3660)
+	const minutes = Math.floor((secondsDifference % 3600) / 60)
+	const seconds = secondsDifference % 60
+	return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds
+		.toString()
+		.padStart(2, "0")}`
 }
 
 function changeBackground(isNight) {
-    const nav = document.querySelector("nav")
-    if (isNight) {
-        document.documentElement.setAttribute("data-theme", "dark")
-        nav.classList.remove("bg-light")
-        nav.classList.add("navbar-dark")
-        nav.classList.add("bg-dark")
-        legendColor = "white"
-    } else {
-        document.documentElement.setAttribute("data-theme", "light")
-        nav.classList.remove("bg-dark")
-        nav.classList.remove("navbar-dark")
-        nav.classList.add("bg-light")
-        legendColor = "black"
-    }
+	const nav = document.querySelector("nav")
+	if (isNight) {
+		document.documentElement.setAttribute("data-theme", "dark")
+		nav.classList.remove("bg-light")
+		nav.classList.add("navbar-dark")
+		nav.classList.add("bg-dark")
+		legendColor = "white"
+	} else {
+		document.documentElement.setAttribute("data-theme", "light")
+		nav.classList.remove("bg-dark")
+		nav.classList.remove("navbar-dark")
+		nav.classList.add("bg-light")
+		legendColor = "black"
+	}
 }
--- room-dashboard/room-dashboard-window.js
+++ room-dashboard/room-dashboard-window.js
@@ -1,152 +1,143 @@
 const start = new Date().setHours(8, 0, 0)
 const end = new Date().setHours(19, 0, 0)
 let now = new Date()
-let isNight = start < now && now < end ? false : true
+let isNight = !(start < now && now < end)
 document.documentElement.setAttribute("data-theme", isNight ? "dark" : "light")
 
 window.onload = function () {
-    document.querySelector("body").style.transitionDuration = "1s"
-    updateClock()
-    setInterval(updateClock, 1000)
-    const slider = document.getElementById("slider")
-    const lightSwitch = document.querySelector(".light")
-    setupSystem(slider).then((response) => {
-        let isLightsOn = response
-        checkLights(isLightsOn)
-        lightSwitch.addEventListener("click", function () {
-            isLightsOn = !isLightsOn
-            checkLights(isLightsOn)
-        })
-    })
-    handleSlider(slider)
-    changeBackground(isNight)
-    setTimeout(function () {
-        document.querySelector(".preload").style.opacity = "0"
-        document.querySelector(".preload").style.zIndex = "-1"
-    }, 500)
+	document.querySelector("body").style.transitionDuration = "1s"
+	updateClock()
+	setInterval(updateClock, 1000)
+	const slider = document.getElementById("slider")
+	const lightSwitch = document.querySelector(".light")
+	setupSystem(slider).then(response => {
+		let isLightsOn = response
+		checkLights(isLightsOn)
+		lightSwitch.addEventListener("click", function () {
+			isLightsOn = !isLightsOn
+			checkLights(isLightsOn)
+		})
+	})
+	handleSlider(slider)
+	changeBackground(isNight)
+	setTimeout(function () {
+		document.querySelector(".preload").style.opacity = "0"
+		document.querySelector(".preload").style.zIndex = "-1"
+	}, 500)
 }
 
 function setupSystem(slider) {
-    return axios.get("logs.json").then((response) => {
-        let windowData = response.data["data"]["window-log"]
-        let lightsData = response.data["data"]["lights-log"]
-        slider.defaultValue = windowData[windowData.length - 1].status
-        slideRollerBlinds(slider.defaultValue)
-        return lightsData[lightsData.length - 1].status == "On" ? true : false
-    })
+	return axios.get("logs.json").then(response => {
+		let windowData = response.data["data"]["window-log"]
+		let lightsData = response.data["data"]["lights-log"]
+		slider.defaultValue = windowData[windowData.length - 1].status
+		slideRollerBlinds(slider.defaultValue)
+		return lightsData[lightsData.length - 1].status == "On"
+	})
 }
 
 function handleSlider(slider) {
-    slider.addEventListener("mousedown", function () {
-        document.getElementById("close").style.transition = "none"
-        slider.addEventListener("mousemove", function () {
-            slideRollerBlinds(this.value)
-        })
-    })
-    slider.addEventListener("mouseup", function () {
-        document.getElementById("close").style.transition = "top 0.5s"
-        slider.removeEventListener("mousemove", function () {
-            slideRollerBlinds(this.value)
-        })
-        updateWindow(this.value)
-    })
-    slider.addEventListener("click", function () {
-        slideRollerBlinds(this.value)
-    })
+	slider.addEventListener("mousedown", function () {
+		document.getElementById("close").style.transition = "none"
+		slider.addEventListener("mousemove", function () {
+			slideRollerBlinds(this.value)
+		})
+	})
+	slider.addEventListener("mouseup", function () {
+		document.getElementById("close").style.transition = "top 0.5s"
+		slider.removeEventListener("mousemove", function () {
+			slideRollerBlinds(this.value)
+		})
+		updateWindow(this.value)
+	})
+	slider.addEventListener("click", function () {
+		slideRollerBlinds(this.value)
+	})
 }
 
 function slideRollerBlinds(value) {
-    document.getElementById("close").style.top = -value * 0.85 + "%"
-    if (value == 0) {
-        document.getElementById("window-status").innerHTML = "closed"
-    } else if (value == 100) {
-        document.getElementById("window-status").innerHTML = "completely open"
-    } else {
-        document.getElementById("window-status").innerHTML =
-            "open at " + value + "%"
-    }
+	document.getElementById("close").style.top = -value * 0.85 + "%"
+	if (value == 0) {
+		document.getElementById("window-status").innerHTML = "closed"
+	} else if (value == 100) {
+		document.getElementById("window-status").innerHTML = "completely open"
+	} else {
+		document.getElementById("window-status").innerHTML = "open at " + value + "%"
+	}
 }
 
 function updateWindow(value) {
-    const request = {
-        content: {
-            status: parseInt(value),
-            start: new Date().toLocaleTimeString([], {
-                hour: "2-digit",
-                minute: "2-digit",
-                second: "2-digit",
-            }),
-            end: new Date().toLocaleTimeString([], {
-                hour: "2-digit",
-                minute: "2-digit",
-                second: "2-digit",
-            }),
-        },
-        type: "window",
-    }
-    axios.post("room-dashboard-history.php", request).then()
+	const request = {
+		content: {
+			status: parseInt(value),
+			start: new Date().toLocaleTimeString([], {
+				hour: "2-digit",
+				minute: "2-digit",
+				second: "2-digit"
+			}),
+			end: new Date().toLocaleTimeString([], {
+				hour: "2-digit",
+				minute: "2-digit",
+				second: "2-digit"
+			})
+		},
+		type: "window"
+	}
+	axios.post("room-dashboard-history.php", request).then()
 }
 
 function checkLights(isLightsOn) {
-    document.getElementById("lights-status").innerHTML = isLightsOn
-        ? "on"
-        : "off"
-    document.getElementById("lights-status").style.color = isLightsOn
-        ? "green"
-        : "red"
-    document.getElementById(
-        isLightsOn ? "light-on" : "light-off"
-    ).style.opacity = "1"
-    document.getElementById(
-        isLightsOn ? "light-off" : "light-on"
-    ).style.opacity = "0"
-    const request = {
-        content: {
-            status: isLightsOn ? "On" : "Off",
-            start: new Date().toLocaleTimeString([], {
-                hour: "2-digit",
-                minute: "2-digit",
-                second: "2-digit",
-            }),
-            end: new Date().toLocaleTimeString([], {
-                hour: "2-digit",
-                minute: "2-digit",
-                second: "2-digit",
-            }),
-        },
-        type: "lights",
-    }
-    axios.post("room-dashboard-history.php", request).then()
+	document.getElementById("lights-status").innerHTML = isLightsOn ? "on" : "off"
+	document.getElementById("lights-status").style.color = isLightsOn ? "green" : "red"
+	document.getElementById(isLightsOn ? "light-on" : "light-off").style.opacity = "1"
+	document.getElementById(isLightsOn ? "light-off" : "light-on").style.opacity = "0"
+	const request = {
+		content: {
+			status: isLightsOn ? "On" : "Off",
+			start: new Date().toLocaleTimeString([], {
+				hour: "2-digit",
+				minute: "2-digit",
+				second: "2-digit"
+			}),
+			end: new Date().toLocaleTimeString([], {
+				hour: "2-digit",
+				minute: "2-digit",
+				second: "2-digit"
+			})
+		},
+		type: "lights"
+	}
+	axios.post("room-dashboard-history.php", request).then()
 }
 
 function changeBackground(isNight) {
-    const night = document.getElementById("night")
-    const day = document.getElementById("day")
-    const nav = document.querySelector("nav")
-    if (isNight) {
-        document.documentElement.setAttribute("data-theme", "dark")
-        day.style.opacity = "0"
-        night.style.opacity = "1"
-        nav.classList.remove("bg-light")
-        nav.classList.add("navbar-dark")
-        nav.classList.add("bg-dark")
-    } else {
-        document.documentElement.setAttribute("data-theme", "light")
-        night.style.opacity = "0"
-        day.style.opacity = "1"
-        nav.classList.remove("bg-dark")
-        nav.classList.remove("navbar-dark")
-        nav.classList.add("bg-light")
-    }
+	const night = document.getElementById("night")
+	const day = document.getElementById("day")
+	const nav = document.querySelector("nav")
+	if (isNight) {
+		document.documentElement.setAttribute("data-theme", "dark")
+		day.style.opacity = "0"
+		night.style.opacity = "1"
+		nav.classList.remove("bg-light")
+		nav.classList.add("navbar-dark")
+		nav.classList.add("bg-dark")
+	} else {
+		document.documentElement.setAttribute("data-theme", "light")
+		night.style.opacity = "0"
+		day.style.opacity = "1"
+		nav.classList.remove("bg-dark")
+		nav.classList.remove("navbar-dark")
+		nav.classList.add("bg-light")
+	}
 }
 
 function updateClock() {
-    const clock = document.getElementById("clock")
-    let currentTime = new Date().toLocaleTimeString("en-US", {
-        hour12: false,
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-    })
-    clock.innerHTML = currentTime
+	const clock = document.getElementById("clock")
+	let currentTime = new Date().toLocaleTimeString("en-US", {
+		hour12: false,
+		hour: "2-digit",
+		minute: "2-digit",
+		second: "2-digit"
+	})
+	clock.innerHTML = currentTime
 }
